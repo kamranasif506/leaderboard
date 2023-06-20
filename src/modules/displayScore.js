351f1509ac437ae192ec93f39d0b7b8b59d419f3
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-06-20 14:22:32 +0500

    resolved linter issues

--- src/modules/displayScore.js
+++ src/modules/displayScore.js
@@ -1,9 +1,9 @@
 function displayScore(list) {
-    let table_html = '';
-    list.forEach((item) => {
-        table_html += ` <tr><td>${item.name}: ${item.score}</td></tr>`;
-    });
-    document.getElementById('table_content').innerHTML = table_html;                    
+  let tableHtml = '';
+  list.forEach((item) => {
+    tableHtml += ` <tr><td>${item.name}: ${item.score}</td></tr>`;
+  });
+  document.getElementById('table_content').innerHTML = tableHtml;
 }
 
 export default displayScore;
