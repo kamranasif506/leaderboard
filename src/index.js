1e0aeb373ea9b4b5f038bfc7f267a37359879b76
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-06-20 16:09:20 +0500

    fetch data from API and show in the leaderboard Score section

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.css';
-import displayScore from './modules/displayScore.js';
+import displayScore  from './modules/displayScore.js';
 
-const list = [
-  { name: 'Name', score: 100 },
-  { name: 'Name', score: 90 },
-  { name: 'Name', score: 70 },
-  { name: 'Name', score: 50 },
-  { name: 'Name', score: 10 },
-];
-displayScore(list);
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const gameId = 'oNNL4c128XACNJjZeGHR';
+const formId = document.getElementById('form');
+
+formId.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log();
+})
+
+displayScore(baseUrl,gameId);
--- src/modules/displayScore.js
+++ src/modules/displayScore.js
@@ -1,9 +1,22 @@
-function displayScore(list) {
-  let tableHtml = '';
-  list.forEach((item) => {
-    tableHtml += ` <tr><td>${item.name}: ${item.score}</td></tr>`;
-  });
-  document.getElementById('table_content').innerHTML = tableHtml;
-}
+const fetchScores = async (baseUrl,gameId) => {
+    let tableHtml = '';
+    const url = baseUrl+"games/"+gameId+"/scores/";
+    console.log(url);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const res = data.result;
+      console.log(res);
+      res.forEach((item) => {
+        tableHtml += `<tr><td>${item.user} : ${item.score}</td></tr>`;
+      });
+      document.getElementById('table_content').innerHTML = tableHtml;
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  }
 
-export default displayScore;
+ 
+  
+ export default fetchScores;
+  
